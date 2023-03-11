d3072a7113815ac4f4d27b6b454cb240b0b13e27
Hasibul Islam <hi006kuet@gmail.com>
2023-03-11 15:36:16 +0600

    The file is commited

---
+++ app/src/main/java/com/example/basiccalculator/conflict.java
@@ -0,0 +1,26 @@
+package com.example.basiccalculator;
+
+public class conflict {
+    int a,b;
+
+    public conflict(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
